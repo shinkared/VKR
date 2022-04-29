9ac1356ec8d4c79e674ed77a1f042d888923fb5d
Валентин Шинкарев <valentin99181@gmail.com>
2022-04-29 15:03:38 +0300

    gitignore

--- mysite/venv/Scripts/activate.bat
+++ mysite/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\PycharmProjects\VKR-1\mysite\venv
+set VIRTUAL_ENV=C:\Users\Win10_Game_OS\source\repos\shinkared\VKR\mysite\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
